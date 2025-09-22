92ef7d6b3fc2a36a7c8a13abfdf7dc7c42afe85e
Kerry Davis <pulsta@gmail.com>
2025-09-22 12:54:24 +1200

    fix: Remove unused analytics import and variable
    - Remove getAnalytics import from firebase/analytics
    - Remove unused analytics variable declaration
    - Fixes TypeScript error: 'analytics' is declared but its value is never read
    - Cleans up firebase.ts to only include used dependencies

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,5 +21,4 @@ export const googleClientId = "632377456958-4caj2mns5u7b4e50s2rjj2kf2oof91k1.app
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
